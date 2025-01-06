3f9580e6001140c60cc20796cbdc08d5d0b46047
wako <izu.wako1408@gmail.com>
2025-01-06 14:31:16 +0000

    適切なコミットメッセージ

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     public function index(Post $post)
     {
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit(2)]);
-    }   //
+    }   
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
@@ -23,8 +23,23 @@ class PostController extends Controller
     public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
+        dd($input);
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
 
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    } 
+
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+
+        return redirect('/posts/' . $post->id);
+    }
+
 }
+
